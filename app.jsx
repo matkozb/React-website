36247ae39611e709767c362d58404c1569cdd4e8
Matthew <140003967+matkozb@users.noreply.github.com>
2024-10-10 13:31:55 +0100

    app.jsx

---
+++ app.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+
+
+function App() {
+
+  const Color="Blue"
+
+  return (
+    <div>
+       <Navbar />
+      <p>Matthews Fave color is {Color}</p>
+      <ul>
+        <li>David</li>
+        <li>Matthew</li>
+        <li>Barnabas</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
