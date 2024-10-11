6d694f447cdf1d276a4a2a63f783225a88f43fe2
Matthew <140003967+matkozb@users.noreply.github.com>
2024-10-11 13:14:09 +0100

    Update navbar.jsx

--- navbar.jsx
+++ navbar.jsx
@@ -1,11 +1,24 @@
+import logo from '../assets/KevinrushLogo.png';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaSquareXTwitter  } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+
 function Navbar(){
 
-    return(
-      <div>
-        <h1>Hello World</h1>
-        <p>Thank you David</p>
+  return(
+    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+      <div className="flex flexshrink-0 items-center">
+        <img src={logo} alt="logo" />
       </div>
-    )
-    }
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin />
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaInstagram />
+      </div>
+    </nav>
+  )
+  }
 
-    export default Navbar
+  export default Navbar
